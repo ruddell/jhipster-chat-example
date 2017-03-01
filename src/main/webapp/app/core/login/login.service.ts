6be37bf5731f1bd60598a39de6ecd336b782aec3
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-02-28 17:40:37 -0800

    set up chat user interface
    replace home.component.html with a message container and input
    add chat logic in home.component.ts
    add event broadcast on logout

--- src/main/webapp/app/core/login/login.service.ts
+++ src/main/webapp/app/core/login/login.service.ts
@@ -3,13 +3,15 @@ import { Injectable } from '@angular/core';
 import { AccountService } from 'app/core/auth/account.service';
 import { AuthServerProvider } from 'app/core/auth/auth-jwt.service';
 import { JhiTrackerService } from 'app/core/tracker/tracker.service';
+import { EventManager } from 'ng-jhipster';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
     constructor(
         private accountService: AccountService,
         private trackerService: JhiTrackerService,
-        private authServerProvider: AuthServerProvider
+        private authServerProvider: AuthServerProvider,
+        private eventManager: EventManager
     ) {}
 
     login(credentials, callback?) {
@@ -40,5 +42,9 @@ export class LoginService {
     logout() {
         this.authServerProvider.logout().subscribe();
         this.accountService.authenticate(null);
+        this.eventManager.broadcast({
+            name: 'logoutSuccess',
+            content: 'Sending Logout Success'
+        });
     }
 }
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { EventManager } from 'ng-jhipster';
+import { ChatService } from '../shared';
 
 import { LoginModalService, AccountService, Account } from 'app/core';
 
@@ -12,25 +13,43 @@ import { LoginModalService, AccountService, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    messages: Array<Object> = [];
+    message = '';
 
     constructor(
         private accountService: AccountService,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: EventManager,
+        private chatService: ChatService
     ) {}
 
     ngOnInit() {
-        this.accountService.identity().then((account: Account) => {
+        this.chatService.connect();
+
+        this.chatService.receive().subscribe(message => {
+            this.messages.push(message);
+        });
+
+        this.accountService.identity().then(account => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        this.registerLogoutSuccess();
     }
 
     registerAuthenticationSuccess() {
         this.eventManager.subscribe('authenticationSuccess', message => {
             this.accountService.identity().then(account => {
                 this.account = account;
+                this.chatService.disconnect();
+                this.chatService.connect();
             });
+        });
+    }
+    registerLogoutSuccess() {
+        this.eventManager.subscribe('logoutSuccess', message => {
+            this.chatService.disconnect();
+            this.chatService.connect();
         });
     }
 
@@ -40,5 +59,13 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    sendMessage(message) {
+        if (message.length === 0) {
+            return;
+        }
+        this.chatService.sendMessage(message);
+        this.message = '';
     }
 }
